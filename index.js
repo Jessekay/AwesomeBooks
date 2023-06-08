1a27fc820ae49019556335d4f6c7b3d84ed50f5a
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-06-08 14:03:19 +0300

    Fix linters 7

--- index.js
+++ index.js
@@ -57,4 +57,5 @@ class BookCollection {
     localStorage.setItem('books', JSON.stringify(this.books));
   }
 }
-new BookCollection();
+const bookCollection = new BookCollection();
+bookCollection.resetForm();
